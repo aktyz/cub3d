3885cf6f65e0d0f533645fb55560a026a664fd03
Zytka <zyta.slowianska@gmail.com>
2025-05-16 22:10:25 +0200

    test: github ruleset

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 22:00:34 by zslowian          #+#    #+#             */
-/*   Updated: 2025/05/16 22:03:46 by zslowian         ###   ########.fr       */
+/*   Updated: 2025/05/16 22:10:09 by zslowian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 int	main(void)
 {
 	ft_printf("Project can be run\n");
+	ft_printf("Testing github main ruleset\n");
 	return (0);
 }
