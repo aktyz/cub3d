a97c4a2ea7e0944d1e00761f44ef86f0050655c1
Honorata Hurnik <hhurnik@c1r3s1.42warsaw.pl>
2025-05-21 16:39:52 +0200

    my notes, dcalculated distance to projection plane at exact moment, updated header

--- cub3d.h
+++ cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hhurnik <hhurnik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 22:02:02 by zslowian          #+#    #+#             */
-/*   Updated: 2025/05/16 22:03:37 by zslowian         ###   ########.fr       */
+/*   Updated: 2025/05/21 16:34:16 by hhurnik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,26 @@
 
 # include "libft/headers/libft.h"
 # include "minilibx-linux/mlx.h"
+# include "math.h"
+
+typedef struct s_player_position
+{
+	int height; //half of grid size = 32 (looks good on the screen)
+	int fov; //how many degrees - 60 
+	int position_x;
+    int position_y;
+	int distance_to_projection_plane; // calculated after each movement
+}	t_player_position;
+
+
+typedef struct s_projection_plane
+{
+	//dimensions of the projection plane = 320 x 200 (resolution of most VGA video cards)
+	//grid_height = 64
+
+	int x;
+	int y;
+
+} t_projection_plane;
 
 #endif
