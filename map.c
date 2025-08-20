0661309b19f7712cf0304eee89f35728a6c41d07
Zytka <zyta.slowianska@gmail.com>
2025-08-20 12:18:08 +0200

    fix: Norm on map.c

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 18:42:30 by zslowian          #+#    #+#             */
-/*   Updated: 2025/08/20 11:48:11 by zslowian         ###   ########.fr       */
+/*   Updated: 2025/08/20 12:17:13 by zslowian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@ void		ft_add_map_token(int *i, char *line, t_cub3d *data);
 void		ft_copy_map_token_to_struct(char *map_line, int *map_row,
 				t_cub3d *data);
 static bool	ft_validate_map_char(char map_char, bool *is_player);
+static bool	ft_check_save_player_info(t_cub3d *data, int *map_row,
+				int char_pos, char *map_line);
+static void	ft_space_out_map_line(int *char_pos, int *map_row, t_cub3d *data);
 
 /**
  * Function run basic check on string read from the .cub file
@@ -60,44 +63,30 @@ void	ft_copy_map_token_to_struct(char *map_line, int *map_row,
 			t_cub3d *data)
 {
 	bool	is_player;
-	char	*ptr;
 	int		char_pos;
 
 	is_player = false;
-	ptr = map_line;
-	char_pos = 0;
+	char_pos = -1;
 	data->map[*map_row] = ft_calloc(data->map_cols + 1, sizeof(char));
 	if (!data->map[*map_row])
 		ft_error(MEM_ERROR, "ft_copy_map_token_to_struct", data);
-	while (char_pos < data->map_cols)
+	while (++char_pos < data->map_cols)
 	{
-		if (!ft_validate_map_char(*ptr, &is_player))
-			ft_error(WRONG_MAP, "ft_save_map - map contains wrong character", data);
-		if (*ptr == '\n')
-		{
-			while (char_pos < data->map_cols - 1)
-			{
-				data->map[*map_row][char_pos] = ' ';
-				char_pos++;
-			}
-		}
+		if (!ft_validate_map_char(*map_line, &is_player))
+			ft_error(WRONG_MAP,
+				"ft_save_map - map contains wrong character", data);
+		if (*map_line == '\n')
+			ft_space_out_map_line(&char_pos, map_row, data);
 		else
-			data->map[*map_row][char_pos] = *ptr;
+			data->map[*map_row][char_pos] = *map_line;
 		if (is_player)
-		{
-			if (data->player.orientation)
-				ft_error(WRONG_MAP, "ft_copy_map_token_to_struct - more than one player", data);
-			data->player.start_row = *map_row;
-			data->player.start_col = char_pos;
-			data->player.orientation = *ptr;
-			is_player = false;
-		}
-		ptr++;
-		char_pos++;
+			is_player = ft_check_save_player_info(data, map_row,
+					char_pos, map_line);
+		map_line++;
 	}
 	data->map[*map_row][char_pos - 1] = '\0';
 	*map_row = *map_row + 1;
-} // Function has more than 25 lines
+}
 
 /**
  * Function checks if map character is valid. It returns:
@@ -125,4 +114,25 @@ static bool	ft_validate_map_char(char map_char, bool *is_player)
 		return (true);
 	}
 	return (false);
+}
+
+static bool	ft_check_save_player_info(t_cub3d *data, int *map_row,
+				int char_pos, char *map_line)
+{
+	if (data->player.orientation)
+		ft_error(WRONG_MAP,
+			"ft_copy_map_token_to_struct - more than one player", data);
+	data->player.start_row = *map_row;
+	data->player.start_col = char_pos;
+	data->player.orientation = *map_line;
+	return (false);
+}
+
+static void	ft_space_out_map_line(int *char_pos, int *map_row, t_cub3d *data)
+{
+	while (*char_pos < data->map_cols - 1)
+	{
+		data->map[*map_row][*char_pos] = ' ';
+		*char_pos = *char_pos + 1;
+	}
 }
